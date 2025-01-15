ecc7d3ad0bc41aeaf16360f3bb788d235a2fe601
Mujeeb Alam <143870536+mujeeb944@users.noreply.github.com>
2025-01-15 14:20:05 +0500

    removed extra tabs

--- content.js
+++ content.js
@@ -1201,18 +1201,6 @@ const RandomCounter = () => {
                   {task.text}
                   <span onClick={() => toggleTask(task.id)} style={taskTextStyle}>
                     {task.text}
-                  </span>
-                  <span onClick={() => toggleTask(task.id)} style={taskTextStyle}>
-                    {task.text}
-                    <span onClick={() => toggleTask(task.id)} style={taskTextStyle}>
-                      {task.text}
-                    </span>
-                    <span onClick={() => toggleTask(task.id)} style={taskTextStyle}>
-                      {task.text}
-                    </span>
-                  </span>
-                  <span onClick={() => toggleTask(task.id)} style={taskTextStyle}>
-                    {task.text}
                     <span onClick={() => toggleTask(task.id)} style={taskTextStyle}>
                       {task.text}
                     </span>
